6443e777f4f1543a95b50e50351e82048217f584
mistermooster11 <mistermooster11@gmail.com>
2026-05-31 09:42:12 +0700

    add sales popup (expires June 7 2026)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/custom/footer/Footer";
 import PageTransition from "@/components/custom/page-transition/PageTransition";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import SalePopup from "@/components/custom/popup/SalePopup"
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -92,6 +93,7 @@ export default function RootLayout({
       </head>
       <body>
         <PageTransition />
+        <SalePopup businessName="O'Connor & Sons Electric" expiryDate="June 7, 2026" trade="electricians" />
         <Header />
         <main>{children}</main>
         <Footer />
